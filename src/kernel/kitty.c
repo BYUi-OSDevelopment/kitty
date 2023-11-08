04d6b03b917728e3cedb614c7fd89b74c983a391
JimTheScientist <aidannpratt@gmail.com>
2023-11-07 18:53:33 -0700

    wowza

--- src/kernel/kitty.c
+++ src/kernel/kitty.c
@@ -3,9 +3,15 @@
 //
 
 #include "kitty.h"
+#include "../driver/video/vga.h"
 
 void kitty() {
     while (1) {
-
+        struct color new_color;
+        new_color.red = 255;
+        new_color.blue = 0;
+        new_color.green = 0;
+        char guh[] = "i am popping off ngl";
+        write_string(1, guh);
     }
 }
--- src/x86-boot/boot-sect.asm
+++ src/x86-boot/boot-sect.asm
@@ -8,6 +8,22 @@
 
 mov [BOOT_DRIVE], dl		; set boot drive to DL
 
+
+load_bootsect: ; load the remaining part of the boot sector (just the text we print) because it's too large to fit in 512 bytes!
+    xor ax, ax			; set ax to 0
+    mov ds, ax			; set data segment (ds) to value of ax (0)
+    cld				; clear direction flag
+    mov ah, 0x02			; read sectors from drive
+    mov al, 63			; specify numbers of sectors in decimal
+    mov dh, 0			; specify drive head
+    mov ch, 0			; start at cylinder 0
+    mov cl, 2			; start at sector 2
+    xor bx, bx			; make entire register 0s
+    mov es, bx			; now bx is starting point in RAM
+    mov bx, 0x7e01			; set bx to end of drive (address 0x7e00)
+
+    int 0x13			; load bios interrupt calls
+
 mov cl, 0
 
 ; clears the screen, sets cursor position + page number
@@ -28,6 +44,7 @@ print_drive_menu: ; put address of LOAD_TEXT into bx!
     inc bx
     cmp [bx], byte 0
     jne print_drive_menu
+check_and_print_x:
     cmp cl, byte 0 ; figure out what is selected, and put the cursor in the right place
     jne check_1
     mov dh, 2
@@ -57,50 +74,58 @@ get_input:
     je move_down
     cmp ah, 0x1C
     je load_kernel
-
 move_up:
     cmp cl, 0
-    je print_clear
+    je get_input
     dec cl
-    jmp print_clear
+    jmp clear_x
 move_down:
     cmp cl, 2
-    je print_clear
+    je get_input
     inc cl
-    jmp print_clear
+    jmp clear_x
+clear_x:
+    mov ah, 0x02
+    mov bh, 0
+    mov dl, 4
+    int 0x10
+    mov ah, 0x0e
+    mov al, ' '
+    int 0x10
+    jmp check_and_print_x
 ; start boot process
 
 load_kernel:
-xor ax, ax			; set ax to 0
-mov ds, ax			; set data segment (ds) to value of ax (0)
-cld				; clear direction flag
-mov ah, 0x02			; read sectors from drive
-mov al, 63			; specify numbers of sectors in decimal
-mov dh, 0			; specify drive head
+    xor ax, ax			; set ax to 0
+    mov ds, ax			; set data segment (ds) to value of ax (0)
+    cld				; clear direction flag
+    mov ah, 0x02			; read sectors from drive
+    mov al, 63			; specify numbers of sectors in decimal
+    mov dh, 0			; specify drive head
 
-cmp cl, byte 0
-je default_drive_set
-cmp cl, byte 1
-je floppy_drive_set
-cmp cl, byte 2
-je hard_drive_set
-default_drive_set:
-mov dl, [BOOT_DRIVE]
-jmp continue_copy
-floppy_drive_set:
-mov dl, 0x00
-jmp continue_copy
-hard_drive_set:
-mov dl, 0x80
-jmp continue_copy
-continue_copy:			; specify drive
-mov ch, 0			; start at cylinder 0
-mov cl, 2			; start at sector 2
-xor bx, bx			; make entire register 0s
-mov es, bx			; now bx is starting point in RAM
-mov bx, 0x1000			; set bx to end of drive (address 0x7e00)
+    cmp cl, byte 0
+    je default_drive_set
+    cmp cl, byte 1
+    je floppy_drive_set
+    cmp cl, byte 2
+    je hard_drive_set
+    default_drive_set:
+    mov dl, [BOOT_DRIVE]
+    jmp continue_copy
+    floppy_drive_set:
+    mov dl, 0x00
+    jmp continue_copy
+    hard_drive_set:
+    mov dl, 0x80
+    jmp continue_copy
+    continue_copy:			; specify drive
+    mov ch, 0			; start at cylinder 0
+    mov cl, 2			; start at sector 2
+    xor bx, bx			; make entire register 0s
+    mov es, bx			; now bx is starting point in RAM
+    mov bx, 0x1000			; set bx to end of drive (address 0x7e00)
 
-int 0x13			; load bios interrupt calls
+    int 0x13			; load bios interrupt calls
 
 enter_protected:
     cli
@@ -138,14 +163,6 @@ gdt_descriptor:
     dd gdt_start
 
 BOOT_DRIVE:	db 0		; define BOOT_DRIVE label
-LOAD_TEXT:
-    ;db 0xA, 0xD,0xA, 0xD,'   [ ] Default Drive',0xA, 0xD,'   [ ] Floppy Drive',0xA, 0xD,'   [ ] Hard Drive',0xA, 0xD
-    ;db ' _                    ',0xA, 0xD
-    ;db '| | __ _ _______ _ __ ',0xA, 0xD
-    ;db '| |/ _` |_  / _ \  __|',0xA, 0xD
-    ;db '| | (_| |/ /  __/ |   ',0xA, 0xD
-    ;db '|_|\__,_/___\___|_|   '
-    db 0		; text
 
 [bits 32] ; code again
 
@@ -264,6 +281,14 @@ jmp 0x1000 ; jump to our kernel :)
 ; the following code populates the first 512 bytes of the drive
 times 510-($-$$) db 0		; for 510 bytes minus the beginning of file, write 0
 dw 0xAA55			; magix number
+LOAD_TEXT:
+    db 0xA, 0xD,0xA, 0xD,'   [ ] Default Drive',0xA, 0xD,'   [ ] Floppy Drive',0xA, 0xD,'   [ ] Hard Drive',0xA, 0xD
+    db ' _                    ',0xA, 0xD
+    db '| | __ _ _______ _ __ ',0xA, 0xD
+    db '| |/ _` |_  / _ \  __|',0xA, 0xD
+    db '| | (_| |/ /  __/ |   ',0xA, 0xD
+    db '|_|\__,_/___\___|_|   '
+    db 0		; text
 
 CODE_SEG equ gdt_code - gdt_start
 DATA_SEG equ gdt_data - gdt_start
