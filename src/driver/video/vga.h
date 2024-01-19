b41531d85d7e546bf5f63f0e3e5e2ff7324cc65b
JimTheScientist <aidannpratt@gmail.com>
2024-01-18 18:51:45 -0700

    added comment for vga3_color preprocessor function, fixed spacing

--- src/driver/video/vga.h
+++ src/driver/video/vga.h
@@ -24,10 +24,15 @@
 #define VGA3_LIGHT_MAGENTA 0b00001101
 #define VGA3_YELLOW 0b00001110
 #define VGA3_WHITE 0b00001111
-
+/*
+ * compile-time function for computing the vga colors.
+ */
 #define vga3_color(FORE, BACK) ((BACK << 4) + FORE)
+
 uint8_t get_vga3_color(uint8_t fore, uint8_t back);
+
 void write_string(uint8_t color, char *string);
+
 void write_string_at(uint8_t color, char *string, uint8_t x, uint8_t y);
 
 #endif //KITTY_VGA_H
